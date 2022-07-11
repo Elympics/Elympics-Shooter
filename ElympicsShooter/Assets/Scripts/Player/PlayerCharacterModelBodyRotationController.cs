68a1884a91612ccd801238a1f3e82d0facc340e2
Mateusz Zawisza <mateusz.zawisza@daftmobile.com>
2022-07-11 16:41:18 +0200

    Fixed character body rotation

--- ElympicsShooter/Assets/Scripts/Player/PlayerCharacterModelBodyRotationController.cs
+++ ElympicsShooter/Assets/Scripts/Player/PlayerCharacterModelBodyRotationController.cs
@@ -6,16 +6,11 @@ using UnityEngine;
 public class PlayerCharacterModelBodyRotationController : ElympicsMonoBehaviour
 {
 	[Header("References:")]
-	[SerializeField] private Transform firstPersonViewContainer = null;
 	[SerializeField] private Transform rotatingBone = null;
 	[SerializeField] private Transform lookAtTarget = null;
-
-	[Header("Parameters:")]
-	[SerializeField] private Vector3 rotationCorrection = new Vector3();
 
 	public void LateUpdate()
 	{
 		rotatingBone.transform.LookAt(lookAtTarget);
-		rotatingBone.transform.rotation = Quaternion.Euler(rotatingBone.transform.rotation.eulerAngles + rotationCorrection);
 	}
 }
